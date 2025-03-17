9da38ecd355b2c6b8c99713941554c22327ac9f5
Leonardo Davi do Nascimento Rodrigues <140842359+ldnrodrigues@users.noreply.github.com>
2025-03-17 14:04:20 -0300

    Add files via upload

--- lib/models/tarefas.dart
+++ lib/models/tarefas.dart
@@ -2,7 +2,18 @@ class Tarefa {
 
   Tarefa ({required this.titulo, required this.dateTime,});
 
+  Tarefa.fromJson(Map<String, dynamic> json)
+  : titulo = json['titulo'],
+    dateTime = DateTime.parse(json['dateTime']);
+
   String titulo;
   DateTime dateTime;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'titulo': titulo,
+      'dateTime': dateTime.toIso8601String(),
+    };
+  }
 
 }
