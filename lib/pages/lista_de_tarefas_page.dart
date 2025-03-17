370e1f43fc957cf01687a9f3df7ae89e0645edd4
Leonardo Davi do Nascimento Rodrigues <140842359+ldnrodrigues@users.noreply.github.com>
2025-03-17 13:13:30 -0300

    Update lista_de_tarefas_page.dart

--- lib/pages/lista_de_tarefas_page.dart
+++ lib/pages/lista_de_tarefas_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lista_de_tarefas/models/tarefas.dart';
 import '../widgets/tarefas_item.dart';
+import '../repositories/todo_repository.dart';
 
 class ListaTarefas extends StatefulWidget {
   ListaTarefas({super.key});
@@ -11,11 +12,25 @@ class ListaTarefas extends StatefulWidget {
 
 class _ListaTarefasState extends State<ListaTarefas> {
   final TextEditingController tarefaController = TextEditingController();
+  final TodoRepository todoRepository = TodoRepository();
+
   List<Tarefa> tarefas = [];
   List<Tarefa> tarefasConcluidas = [];
 
   Tarefa? deletedTarefa;
   int? deletedTarefaPos;
+
+  String? errorText;
+
+  @override
+  void initState() {
+    super.initState();
+    todoRepository.getTodoList().then((value) {
+      setState(() {
+        tarefas = value;
+      });
+    });
+  }
 
   void onDelete(Tarefa tarefa) {
     deletedTarefa = tarefa;
@@ -24,6 +39,7 @@ class _ListaTarefasState extends State<ListaTarefas> {
     setState(() {
       tarefas.remove(tarefa);
     });
+    todoRepository.saveTodoList(tarefas);
 
     ScaffoldMessenger.of(context).clearSnackBars();
     ScaffoldMessenger.of(context).showSnackBar(
@@ -41,7 +57,9 @@ class _ListaTarefasState extends State<ListaTarefas> {
         backgroundColor: Color(0xFF181818),
         padding: const EdgeInsets.symmetric(horizontal: 10.0),
         behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
         action: SnackBarAction(
           label: 'Desfazer',
           textColor: Color(0xFFD4A200),
@@ -49,6 +67,7 @@ class _ListaTarefasState extends State<ListaTarefas> {
             setState(() {
               tarefas.insert(deletedTarefaPos!, deletedTarefa!);
             });
+            todoRepository.saveTodoList(tarefas);
           },
         ),
       ),
@@ -56,42 +75,65 @@ class _ListaTarefasState extends State<ListaTarefas> {
   }
 
   void onEdit(Tarefa tarefa) {
-    TextEditingController editController = TextEditingController(text: tarefa.titulo);
+    TextEditingController editController = TextEditingController(
+      text: tarefa.titulo,
+    );
 
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text("Editar Tarefa",
-          style: TextStyle(color: Colors.white),
-          ),
+          title: Text("Editar Tarefa", style: TextStyle(color: Colors.white)),
           backgroundColor: Color(0xFF181818),
           content: TextField(
             controller: editController,
             decoration: InputDecoration(
               labelText: "Nova descrição",
               border: OutlineInputBorder(),
+              errorText: '',
             ),
           ),
           actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text("Cancelar"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  tarefa.titulo = editController.text;
-                });
-                Navigator.pop(context);
-              },
-              child: Text("Salvar"),
-            ),
-          ],
-        );
-      },
-    );
-  }
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Row(
+                    children: [
+                      Icon(Icons.cancel_outlined, color: Color(0xFFD4A200)),
+                      SizedBox(width: 5),
+                      Text("Cancelar",
+                          style: TextStyle(color: Color(0xFFD4A200))),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          tarefa.titulo = editController.text;
+                        });
+                        todoRepository.saveTodoList(tarefas);
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(backgroundColor: Color(0xFFD4A200)),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Icon(Icons.save, color: Colors.white),
+                          SizedBox(width: 5),
+                          Text("Salvar",
+                            style: (TextStyle(color: Colors.white)),),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          );
+        }
 
   void showDeleteTarefasConfirmationDialog() {
     showDialog(
@@ -99,51 +141,59 @@ class _ListaTarefasState extends State<ListaTarefas> {
       builder: (context) {
         return AlertDialog(
           backgroundColor: Color(0xFF181818),
-          title: Text(
-            "Limpar Tudo?",
-            style: TextStyle(color: Colors.white),
-          ),
+          title: Text("Limpar Tudo?", style: TextStyle(color: Colors.white)),
           content: Text(
             'Tem certeza que deseja deletar todas as tarefas?',
             style: TextStyle(color: Color(0xFFB0B0B0)),
           ),
           actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              style: TextButton.styleFrom(
-                backgroundColor: Color(0xFFD32F2F),
-                foregroundColor: Colors.white,
-              ),
-              child: Text("Cancelar"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  tarefas.clear();
-                });
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.transparent,
-                shadowColor: Colors.transparent,
-              ),
-              child: Text(
-                "Limpar Tudo",
-              style: TextStyle(color: Color(0xFFD4A200)),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.transparent,
+                    shadowColor: Colors.transparent,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.cancel_outlined, color: Color(0xFFD4A200)),
+                      SizedBox(width: 5),
+                      Text("Cancelar",
+                        style: TextStyle(color: Color(0xFFD4A200)),
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      tarefas.clear();
+                    });
+                    todoRepository.saveTodoList(tarefas);
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFD32F2F),
+                    foregroundColor: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Icon(Icons.delete_forever, color: Colors.white),
+                      SizedBox(width: 3),
+                      Text("Limpar Tudo",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         );
       },
     );
-  }
-
-  void tarefaConcluida(Tarefa tarefa) {
-    setState(() {
-      tarefas.remove(tarefa);
-    })
-
-    ;
   }
 
   @override
@@ -173,10 +223,15 @@ class _ListaTarefasState extends State<ListaTarefas> {
             ),
           ),
           elevation: 4,
-          leading: IconButton(
-            icon: Icon(Icons.task, color: Colors.white, size: 28), // Ícone representativo do app
-            onPressed: () {},
-          ),
+          // leading: IconButton(
+          // icon: Image.asset(
+          // 'assets/images/app_tarefas.png',
+          // width: 100,
+          // height: 100,
+          // fit: BoxFit.cover,
+          // ),
+          // onPressed: () {},
+          // ),
         ),
         body: Padding(
           padding: const EdgeInsets.all(16),
@@ -204,11 +259,15 @@ class _ListaTarefasState extends State<ListaTarefas> {
                         filled: true,
                         fillColor: Color(0xFF222222),
                         focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Color(0xFFD4A200), width: 2.0),
+                          borderSide: BorderSide(
+                            color: Color(0xFFD4A200),
+                            width: 2.0,
+                          ),
                         ),
                         border: OutlineInputBorder(),
                         labelText: 'Adicione uma Tarefa',
                         hintText: 'Ex. Estudar',
+                        errorText: errorText,
                         floatingLabelStyle: TextStyle(color: Color(0xFFD4A200)),
                       ),
                       style: TextStyle(color: Colors.white),
@@ -226,20 +285,30 @@ class _ListaTarefasState extends State<ListaTarefas> {
                     ),
                     onPressed: () {
                       String text = tarefaController.text;
+
+                      if (text.isEmpty) {
+                        setState(() {
+                          errorText = 'O campo não pode estar vazio';
+                        });
+                        return;
+                      }
+
+                      setState(() {
+                        errorText = null;
+                      });
+
                       setState(() {
                         Tarefa novaTarefa = Tarefa(
                           titulo: text,
                           dateTime: DateTime.now(),
                         );
                         tarefas.add(novaTarefa);
+                        errorText = null;
                       });
                       tarefaController.clear();
+                      todoRepository.saveTodoList(tarefas);
                     },
-                    child: Icon(
-                      Icons.add,
-                      size: 25,
-                      color: Colors.white,
-                    ),
+                    child: Icon(Icons.add, size: 25, color: Colors.white),
                   ),
                 ],
               ),
@@ -281,7 +350,7 @@ class _ListaTarefasState extends State<ListaTarefas> {
                       'Limpar tudo',
                       style: TextStyle(color: Colors.white),
                     ),
-                  )
+                  ),
                 ],
               ),
             ],
